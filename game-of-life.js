d55d6a8c34311ed08162ffefb57e87ed8cf261fe
Nathan Pope <NathanWPope@hotmail.co.uk>
2021-07-01 19:29:27 +0100

    Manual Iterate Life Cycle Added functionality for manually iterating life cycles

--- game-of-life.js
+++ game-of-life.js
@@ -1,59 +1,105 @@
 class GameOfLife {
   constructor() {
-    this.gridHeight = 500; // Grid height
-    this.gridWidth = 500; // Grid width
+    this.gridWidth = 1280; // Grid width
+    this.gridHeight = 720; // Grid height
     this.gridCell = 10; // Grid cell height and width
     this.rows = this.gridWidth / this.gridCell; // Calculates how many cells fit into the grid width
     this.cols = this.gridHeight / this.gridCell; // Calculates how many cells fit into the grid height
-    this.nextIteration = [];
-    this.currentIteration = [];
-    this.initialiseGrid(); // Fills currentIteration and nextIteration arrays with rows, columns, and empty cells
-    this.render(); // Render grid canvas
-    this.seedGrid(); // Generates random grid seed
+    this.grid = []; // Fills grid array with rows, columns, and empty cells
+    this.initialiseGrid();
   }
 
   // Initialises grid arrays with dead cells
   initialiseGrid() {
+    let grid = [];
     // Initialise rows and columns with dead cells
     for (let row = 0; row < this.rows; row++) {
       // Loop through grid rows
-      this.nextIteration[row] = []; // Initialise column array to be iterated through
+      grid[row] = []; // Initialise column array to be iterated through
       for (let col = 0; col < this.cols; col++) {
         // Loop through grid columns
-        this.nextIteration[row][col] = false; // Sets initial cell state to false / dead
+        grid[row][col] = 0; // Sets initial cell state to 0 / dead
       }
     }
-    this.currentIteration = this.nextIteration; // Initialises currentIteration array
+    this.grid = grid;
+    this.render(); // Render grid canvas
   }
 
   // Seed grid with random cell states
   seedGrid() {
+    let grid = this.grid;
     // Randomise cell states and return updated grid
-    for (let row = 0; row < this.rows; row++) {
-      for (let col = 0; col < this.cols; col++) {
-        this.nextIteration[row][col] = Math.random() < 0.5; // 50% chance for cell to be true or false
+    for (let row = 0; row < grid.length; row++) {
+      for (let col = 0; col < grid[row].length; col++) {
+        grid[row][col] = Math.random() > 0.5 ? 1 : 0; // 50% chance for cell to be true or false
       }
     }
-    this.updateLifeCycle(); // Updates current iteration array to next life cycle
+    this.grid = grid;
+    this.render(); // Render grid canvas
   }
 
-  // Updates the currentIteration array with next life cycle, re-renders the grid
+  // Determines cell states for next life cycle
   updateLifeCycle() {
-    this.currentIteration = this.nextIteration; // Updates current life cycle to next life cycle
-    this.render(); // Updated render of the grid and cells
+    const newGrid = this.grid.map((arr) => [...arr]); // Duplicate grid array
+    // Select a cell and count its neighbours
+    for (let row = 0; row < this.grid.length; row++) {
+      for (let col = 0; col < this.grid[row].length; col++) {
+        let neighbourCount = 0; // Tally of neighbouring cells
+
+        // Loop through cell neighbours
+        for (let i = -1; i <= 1; i++) {
+          // Cell row
+          for (let j = -1; j <= 1; j++) {
+            // Cell column
+            // Prevent cell counting itself as a neighbour
+            if (i === 0 && j === 0) {
+              continue;
+            }
+            // Check that cell neighbour position does not exceed grid boundries
+            if (
+              row + i >= 0 &&
+              col + j >= 0 &&
+              row + i < this.rows &&
+              col + j < this.cols
+            ) {
+              neighbourCount += this.grid[row + i][col + j]; // Updates neighbour count variable by 1
+            }
+          }
+        }
+        // Apply rules
+        // If a live cell has less than 2 or more than 3 neighbours it dies
+        if (
+          (this.grid[row][col] === 1 && neighbourCount < 2) ||
+          neighbourCount > 3
+        ) {
+          newGrid[row][col] = 0;
+        }
+        // If a live cell has 2 or 3 neighbours it continues to live
+        else if (
+          (this.grid[row][col] === 1 && neighbourCount === 2) ||
+          neighbourCount === 3
+        ) {
+          newGrid[row][col] = 1;
+        }
+        // If a dead cell has 3 neighbours then it lives
+        else if (this.grid[row][col] === 0 && neighbourCount === 3) {
+          newGrid[row][col] = 1;
+        }
+      }
+    }
+    this.grid = newGrid;
+    this.render(); // Render grid canvas
   }
 
   //   Renders canvas and cells
   render() {
-    const grid = this.currentIteration;
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
-
     canvas.width = this.gridWidth; // Set grid canvas width
     canvas.height = this.gridHeight; // Set grid canvas height
 
-    for (let row = 0; row < grid.length; row++) {
-      for (let col = 0; col < grid[row].length; col++) {
+    for (let row = 0; row < this.rows; row++) {
+      for (let col = 0; col < this.cols; col++) {
         ctx.beginPath();
         ctx.rect(
           row * this.gridCell,
@@ -61,24 +107,43 @@ class GameOfLife {
           this.gridCell,
           this.gridCell
         );
-        ctx.fillStyle = grid[row][col] ? "green" : "white";
+        ctx.fillStyle = this.grid[row][col] ? "green" : "white";
         ctx.fill();
         ctx.stroke();
       }
     }
-    requestAnimationFrame(this.render); // Animates iteration changes to grid
   }
 }
 
 window.onload = () => {
   const game = new GameOfLife();
-  const startPauseBtn = document.getElementById("start-pause-btn");
+  const startStopBtn = document.getElementById("start-stop-btn");
   const nextIterationBtn = document.getElementById("next-iteration-btn");
   const seedBtn = document.getElementById("seed-btn");
   const resetBtn = document.getElementById("reset-btn");
 
-  // HTML seed grid button event listener
-  // Calls the GameOfLife seedGrid function
+  // window.setInterval(() => {
+  //   game.updateLifeCycle();
+  // }, 100);
+
+  startStopBtn.addEventListener(
+    "click",
+    function () {
+      game.updateLifeCycle();
+    },
+    false
+  );
+
+  // Button Event Listeners
+  nextIterationBtn.addEventListener(
+    "click",
+    function () {
+      game.updateLifeCycle();
+    },
+    false
+  );
+
+  // HTML seed grid button event listener. Calls the GameOfLife seedGrid function.
   seedBtn.addEventListener(
     "click",
     function () {
@@ -86,11 +151,11 @@ window.onload = () => {
     },
     false
   );
-  //   resetBtn.addEventListener(
-  //     "click",
-  //     function () {
-  //       game.initialiseGrid();
-  //     },
-  //     false
-  //   );
+  resetBtn.addEventListener(
+    "click",
+    function () {
+      game.initialiseGrid();
+    },
+    false
+  );
 };
